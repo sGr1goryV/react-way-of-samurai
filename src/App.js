89f724c562f949ae442144132c93a934e8acbfe5
sGr1goryV <Grixan886@gmail.com>
2023-05-20 22:50:18 +0400

    step 1 itk

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import './zeroing.css';
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="App">
+        <ul>
+          <li>BMW</li>
+          <li>MERCEDEC</li>
+          <li>AUDi</li>
+        </ul>
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
