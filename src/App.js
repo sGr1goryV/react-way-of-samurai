09301a99dd343a47e79cc3215101308a34e10c14
sGr1goryV <Grixan886@gmail.com>
2023-05-30 19:42:58 +0400

    step 3 itk

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import './css/App.css';
-import './css/zeroing.css'
-import Midle from './blocks/Midle';
-import Header from './blocks/Header';
+import './components/zeroing.css'
+import './components/main.css'
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import Profile from './components/Profile/Profile'
+
 
 function App() {
   return (
   <div className='container'>
     <Header />
-    <Midle />
+    <Navbar />
+    <Profile />
   </div>
     
   );
